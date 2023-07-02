4245dd2c821aff52b5a07c951fc92fa4479d9848
Fauzan Falah <fauzancodekop@gmail.com>
2023-07-03 04:15:22 +0700

    Update crud_array.php

--- crud_array.php
+++ crud_array.php
@@ -25,3 +25,31 @@ $html_array .= '    "'.$col['name'].'",
         ';   
             }
 $html_array .= '];';
+$html_array .= '
+
+/* FOR API */ 
+$data = [
+        ';
+            for ($i = 0; $i < $kolom->columnCount(); $i++) {
+                $col = $kolom->getColumnMeta($i);
+                $col['name'];
+                // echo $col['native_type'].' =>'.$col['name'].'<br>';
+                $label = ucfirst(preg_replace('/[^a-zA-Z0-9\']/', ' ', $col['name']));
+$html_array .= '    "'.$col['name'].'" => $row["'.$col['name'].'"],
+        ';   
+            }
+$html_array .= '];';
+$html_array .= '
+
+/* FOR API OBJECT */ 
+$data = [
+        ';
+            for ($i = 0; $i < $kolom->columnCount(); $i++) {
+                $col = $kolom->getColumnMeta($i);
+                $col['name'];
+                // echo $col['native_type'].' =>'.$col['name'].'<br>';
+                $label = ucfirst(preg_replace('/[^a-zA-Z0-9\']/', ' ', $col['name']));
+$html_array .= '    "'.$col['name'].'" => $row->'.$col['name'].',
+        ';   
+            }
+$html_array .= '];';
